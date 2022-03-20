76976b64eb30f01c6907698fb4857a46d4912bcc
babuaditya97 <adybabu11@gmail.com>
2022-03-20 11:06:27 +0530

    facial ladmark detection updated

---
+++ forehead_landmark.py
@@ -0,0 +1,36 @@
+import cv2
+import mediapipe as mp
+
+# Face Mesh
+mp_face_mesh = mp.solutions.face_mesh
+face_mesh = mp_face_mesh.FaceMesh()
+
+cap = cv2.VideoCapture(0)
+
+while True:
+    # Image
+    ret, image = cap.read()
+    if ret is not True:
+        break
+    height, width, _ = image.shape
+    print("Height, width", height, width)
+    rgb_image = cv2.cvtColor(image, cv2.COLOR_BGR2RGB)
+
+    # Facial landmarks
+    result = face_mesh.process(rgb_image)
+    try:
+        for facial_landmarks in result.multi_face_landmarks:
+            # for i in range(0, 468):
+            pt1 = facial_landmarks.landmark[10]
+            x = int(pt1.x * width)
+            y = int(pt1.y * height)
+
+            cv2.circle(image, (x, y), 2, (100, 100, 0), -1)
+            cv2.putText(image, str(10), (x, y), 0, 1, (0, 0, 0))
+    except:
+        pass
+
+    cv2.imshow("Image", image)
+    c = cv2.waitKey(1)
+    if c == 27:
+        break
